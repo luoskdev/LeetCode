f5a86047e44e83c76869093841e464eec8c90ba6
luoskdev <luoskdev@gmail.com>
2013-10-06 01:07:41 +0800

    Add : MedianofTwoSortedArrays.h
    add the solution to the problem of MedianofTwoSortedArrays

---
+++ MedianofTwoSortedArrays.h
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double findMedianSortedArrays(int A[], int m, int B[], int n) 
+    {
+        double res = 0;
+        int i = 0, j = 0, k = (m + n + 1) / 2;
+        int first = 0, second = 0;
+        while (k >= 0)
+        {
+            second = first;
+            if (A[i] <= B[j] && i < m) first = A[i++];
+            else if (A[i] > B[j] && j < n) first = B[j++];
+            else if (i == m) first = B[j++];
+            else first = A[i++];
+            k--;
+        }
+        
+        if ((m + n) % 2 == 0) res = (first + second) / 2.0;
+        else res = second;
+        
+        return res;
+    }
+};
